a816f3057cf83953b7b6e6d37e9e2bcca721a1da
ArmanGrewal007 <armansinghgrewal@gmail.com>
2025-03-20 20:11:52 +0530

    modified vite-config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from "path";
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react(), ghPages()],
-    base: mode === "production" ? "/./" : "/",
+    base: mode === "production" ? "/chatty" : "/",
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
